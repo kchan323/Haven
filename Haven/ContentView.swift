faf5d756a0ef6b03fe9d7bced3d617fad3538ae6
Katelin Chan <ktc33@cornell.edu>
2019-11-17 16:59:27 -0500

    Initial Commit

---
+++ Haven/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Haven
+//
+//  Created by Katelin Chan on 11/17/19.
+//  Copyright © 2019 Katelin Chan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
